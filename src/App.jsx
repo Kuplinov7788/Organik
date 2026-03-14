b9709462f38fa8f5b2b51c5a102435456b05d973
diyor011 <hamidullasunatullayev2005@gmail.com>
2026-03-14 14:48:36 +0500

    shop

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
+
 
 const App = () => {
   return (
     <div>
       <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Home from './pages/Home.jsx';
+import ShopSingle from "./pages/ShopSingle.jsx";
 
 const router = createBrowserRouter([
   {
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
       {
         path: "/",
         element:<Home />
+      },
+       {
+        path: "/shop/:id",
+        element: <ShopSingle />
       }
     ]
   },
